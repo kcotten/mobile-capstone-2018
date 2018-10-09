cc46f89b7801250ae7b4879a5e7c8ed5e4420d37
Treatyoubetter <zyang26@ucmerced.edu>
2018-10-09 16:53:29 -0700

    Basic test of welcome page(included background and a button

--- app/src/main/java/com/iremember/iremember/welcome_screen.java
+++ app/src/main/java/com/iremember/iremember/welcome_screen.java
@@ -2,12 +2,32 @@ package com.iremember.iremember;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class welcome_screen extends AppCompatActivity {
+    private static final String TAG = "Hahaha";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        //Wire up the button to do something
+        //..get the button
+        Button Login = (Button) findViewById(R.id.button);
+        //..set what happens when the user clicks
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"This is iRemember!");
+                Toast.makeText(getApplicationContext(),"Check!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+        });
     }
 }
+
