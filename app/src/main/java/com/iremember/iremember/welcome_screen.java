101f90fe3154084f41692c3cd31f00af01419ed9
kcotten <kris.cotten@gmail.com>
2018-10-09 10:27:54 -0700

    Initial project creation

---
+++ app/src/main/java/com/iremember/iremember/welcome_screen.java
@@ -0,0 +1,13 @@
+package com.iremember.iremember;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
