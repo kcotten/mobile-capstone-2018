bf27587093feb11de3c4c1227c56ea66a55ba258
Kris Cotten <admin@kriscotten.com>
2018-10-10 12:28:38 -0700

    implementing lifecycle support

--- app/src/main/java/com/iremember/iremember/welcome_screen.java
+++ app/src/main/java/com/iremember/iremember/welcome_screen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class welcome_screen extends AppCompatActivity {
@@ -15,10 +16,7 @@ public class welcome_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
-        //Wire up the button to do something
-        //..get the button
         Button Login = (Button) findViewById(R.id.button);
-        //..set what happens when the user clicks
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +26,92 @@ public class welcome_screen extends AppCompatActivity {
             }
 
         });
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_state_change, menu);
+        return true;
+    }
+    */
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    */
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        final EditText email = (EditText) findViewById(R.id.email);
+        CharSequence userEmail = email.getText();
+        outState.putCharSequence("savedEmail", userEmail);
+        final EditText password = (EditText) findViewById(R.id.password);
+        CharSequence userPassword = password.getText();
+        outState.putCharSequence("savedPassword", userPassword);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+        final EditText email = (EditText) findViewById(R.id.email);
+        CharSequence userEmail = savedInstanceState.getCharSequence("savedEmail");
+        email.setText(userEmail);
+        final EditText password = (EditText) findViewById(R.id.password);
+        CharSequence userPassword = savedInstanceState.getCharSequence("savedPassword");
+        password.setText(userPassword);
     }
 }
 
