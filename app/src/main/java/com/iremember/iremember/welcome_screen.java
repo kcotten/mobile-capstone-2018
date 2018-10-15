7493d27bfaf6438a335a95a5f83de1f1476996a4
Kris Cotten <admin@kriscotten.com>
2018-10-14 19:32:59 -0700

    Firebase Authentication boilerplate UI, followed a tutorial but isn't working. Commit here to save my place

--- app/src/main/java/com/iremember/iremember/welcome_screen.java
+++ app/src/main/java/com/iremember/iremember/welcome_screen.java
@@ -1,97 +1,141 @@
 package com.iremember.iremember;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.quickstart;
 import android.app.ProgressDialog;
 
-public class welcome_screen extends AppCompatActivity {
-    private static final String TAG = "Hahaha";
+import java.util.Arrays;
+import java.util.List;
 
-    private FirebaseAuth mAuth;
+class Welcome_screen extends AppCompatActivity {
 
-    private void updateUI(FirebaseUser user) {
-        hideProgressDialog();
-        if (user != null) {
-            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+    private static final String TAG = "Welcome_screen";
+    private static int RC_SIGN_IN = 100;
+    private FirebaseAuth.AuthStateListener AuthListener;
+    private FirebaseAuth Auth;
+    TextView email;
+    TextView userName;
+    Button button1;
+    Button button2;
 
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-        } else {
-            mStatusTextView.setText(R.string.signed_out);
-            mDetailTextView.setText(null);
-
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
-        }
-    }
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+        for (String provider : AuthUI.SUPPORTED_PROVIDERS) {
+            Log.i(this.getClass().getName(), provider);
+        }
 
-        Button Login = (Button) findViewById(R.id.button);
-        Login.setOnClickListener(new View.OnClickListener() {
+        Auth     = FirebaseAuth.getInstance();
+        button1  = findViewById(R.id.button1);
+        button2  = findViewById(R.id.button2);
+        email    = findViewById(R.id.email);
+        userName = findViewById(R.id.user);
+
+        AuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Check!", Toast.LENGTH_SHORT)
-                        .show();
-                Log.i(TAG,"Login");
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                updateUI();
             }
-
-        });
-
+        };
         Log.i(TAG, "onCreate");
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_state_change, menu);
-        return true;
-    }
-    */
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void updateUI() {
+        FirebaseUser user = Auth.getCurrentUser();
+        if(user == null) {
+            button1.setVisibility(View.VISIBLE);
+            button2.setVisibility(View.GONE);
+            email.setVisibility(View.GONE);
+            // change password to user?
+            userName.setVisibility(View.GONE);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else {
+            button1.setVisibility(View.GONE);
+            button2.setVisibility(View.VISIBLE);
+            email.setVisibility(View.VISIBLE);
+            // change password to user?
+            userName.setVisibility(View.VISIBLE);
+
+            userName.setText(user.getDisplayName());
+            email.setText(user.getEmail());
+            // load profile image? have to follow a whole process
+            // Picasso.with(ActivityFUIAuth.this).load(user.getPhotoUrl()).into(imgProfile);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    */
-    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG,"User signed in with " + response.getProviderType());
+                updateUI();
+            } else {
+                updateUI();
+            }
+        }
+    }
+
+    public void signIn(View v) {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    public void deleteAccount(View v) {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                }
+        });
+
+    }
+
+    public void signOut(View view) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(Welcome_screen.this, "signed out succesfully ... ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        Auth.addAuthStateListener(AuthListener);
 
         Log.i(TAG, "onStart");
     }
@@ -130,23 +174,11 @@ public class welcome_screen extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.i(TAG, "onSaveInstanceState");
-        final EditText email = (EditText) findViewById(R.id.email);
-        CharSequence userEmail = email.getText();
-        outState.putCharSequence("savedEmail", userEmail);
-        final EditText password = (EditText) findViewById(R.id.password);
-        CharSequence userPassword = password.getText();
-        outState.putCharSequence("savedPassword", userPassword);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.i(TAG, "onRestoreInstanceState");
-        final EditText email = (EditText) findViewById(R.id.email);
-        CharSequence userEmail = savedInstanceState.getCharSequence("savedEmail");
-        email.setText(userEmail);
-        final EditText password = (EditText) findViewById(R.id.password);
-        CharSequence userPassword = savedInstanceState.getCharSequence("savedPassword");
-        password.setText(userPassword);
     }
 }
