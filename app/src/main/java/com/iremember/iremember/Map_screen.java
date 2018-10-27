8220a168f925271c21d7dcfa86fa6bb658000a5c
Kris Cotten <admin@kriscotten.com>
2018-10-27 12:09:26 -0700

    Beginning get current activity

--- app/src/main/java/com/iremember/iremember/Map_screen.java
+++ app/src/main/java/com/iremember/iremember/Map_screen.java
@@ -2,12 +2,24 @@ package com.iremember.iremember;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,19 +30,40 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class Map_screen extends FragmentActivity implements OnMapReadyCallback {
+public class Map_screen extends FragmentActivity implements
+        OnMapReadyCallback {
 
     private static final String TAG = "Map_screen";
     private static final int LOCATION_REQUEST_CODE = 101;
 
     private GoogleMap mMap;
+    private Location mLastKnownLocation;
+    private boolean mLocationPermissionGranted;
+
+    FusedLocationProviderClient mFusedLocationProviderClient;
+    GeoDataClient mGeoDataClient;
+    PlaceDetectionClient mPlaceDetectionClient;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_screen);
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -47,20 +80,27 @@ public class Map_screen extends FragmentActivity implements OnMapReadyCallback {
         if (mMap != null) {
             int permission = ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION);
+            int permission1 = ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION);
 
-            if (permission == PackageManager.PERMISSION_GRANTED) {
+            if (permission == PackageManager.PERMISSION_GRANTED && permission1 == PackageManager.PERMISSION_GRANTED) {
                 mMap.setMyLocationEnabled(true);
             } else {
                 requestPermission(
                         Manifest.permission.ACCESS_FINE_LOCATION,
                         LOCATION_REQUEST_CODE);
+                requestPermission(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        LOCATION_REQUEST_CODE);
             }
+
         }
 
         mapSettings.setZoomControlsEnabled(true);
         mapSettings.setCompassEnabled(true);
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        /*
         LatLng UCSC = new LatLng(36.9916, -122.0583);
         Marker ucsc = mMap.addMarker(new MarkerOptions()
                 .position(UCSC)
@@ -68,7 +108,40 @@ public class Map_screen extends FragmentActivity implements OnMapReadyCallback {
                 .snippet("University of California Santa Cruz"));
         // mMap.moveCamera(CameraUpdateFactory.newLatLng(UCSC));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSC,13));
+        */
     }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
 
     protected void requestPermission(String permissionType,
                                      int requestCode) {
