3b414c73fd3437764ac8fb17099c84c941bdb529
Kris Cotten <admin@kriscotten.com>
2018-12-01 16:49:01 -0800

    Images now working in high res

--- app/src/main/java/com/iremember/iremember/Images_screen.java
+++ app/src/main/java/com/iremember/iremember/Images_screen.java
@@ -13,6 +13,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
+import android.support.v4.BuildConfig;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -82,6 +83,8 @@ public class Images_screen extends AppCompatActivity {
     String notes2db, desc2db;
     String firebaseImage;
     double lat, lng;
+    String mCurrentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,18 +102,53 @@ public class Images_screen extends AppCompatActivity {
 
         mTakepicture.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //file = getOutputMediaFile();
-                //CameraUri = Uri.fromFile(file); // create
-                //intent.putExtra(MediaStore.EXTRA_OUTPUT, CameraUri);
-                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                dispatchTakePictureIntent();
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent, CAMERA_REQUEST_CODE);
             }
         });
 
 
         startListener();
 
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            file = null;
+            try {
+                file = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (file != null) {
+                CameraUri = FileProvider.getUriForFile(this,
+                        "com.iremember.iremember.android.fileprovider",
+                        file);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, CameraUri);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
     }
 
     /*
@@ -169,11 +207,24 @@ public class Images_screen extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == CAMERA_REQUEST_CODE){
             if (resultCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                mImageView.setImageBitmap(imageBitmap);
-                encodeBitmapAndSaveToFirebase(imageBitmap);
+                //Bundle extras = data.getExtras();
+                //Bitmap imageBitmap = (Bitmap) extras.get("data");
+                //mImageView.setImageBitmap(imageBitmap);
+                //encodeBitmapAndSaveToFirebase(imageBitmap);
+                //CameraUri = data.getParcelableExtra();
+                Log.i(TAG, String.valueOf(CameraUri));
                 Toast.makeText(this, "Picture saved to:\n" + CameraUri, Toast.LENGTH_LONG).show();
+
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), CameraUri);
+                    mImageView.setImageBitmap(bitmap);
+                    encodeBitmapAndSaveToFirebase(bitmap);
+                    Log.i(TAG,"Success");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Picture snapping cancelled.", Toast.LENGTH_LONG).show();
             } else {
